1fedcba498bcddd5fd5621492b33c68d79a3f297
Fabio Gadau <fabiogadau92@gmail.com>
2020-03-31 15:44:40 +0200

    ok

--- script/main.js
+++ script/main.js
@@ -1,1 +1,24 @@
 console.log('Js ok');
+// CHIEDERE IL COGNOME ALL'UTENTE
+// Referenze
+var lista = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Verdi'];
+var cognomi = document.getElementById('cognomi');
+var listaCognomi = document.getElementById('lista-cognomi');
+var btnAggiungi = document.getElementById('btn-aggiungi');
+
+btnAggiungi.addEventListener('click',
+  // INSERIRLO IN UN ARRAY CONTENENTE ALTRI COGNOMI
+  function() {
+    lista.push(cognomi.value);
+    lista.sort();
+
+    // STAMPARE LA LISTA ORDINATA ALFABETICAMENTE
+    // STAMPARE LA POSIZIONE DEL NUOVO UTENTE NELLA LISTA
+    var items = '';
+    for (i = 0; i < lista.length; i++) {
+      items += '<li>' + lista[i] + '</li>';
+
+      listaCognomi.innerHTML = items + 'La posizione del tuo cognome nella lista è: ' + lista.indexOf(cognomi);
+    }
+  }
+);
